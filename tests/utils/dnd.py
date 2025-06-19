d192c16327aaad191fd7a757c89b7365571b14d3
PavlovDaniil <pavlovDaniil492@gmail.com>
2025-06-19 23:24:00 +0300

    добавление тестов

--- tests/utils/dnd.py
+++ tests/utils/dnd.py
@@ -1,3 +1,5 @@
 from bot.utils.dnd_get import get_classes
 
-print(get_classes())
+def test_get_calsses():
+    assert get_classes() == ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+
